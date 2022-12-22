7c57d41e8cebfe3047274257537af0ca258aa98b
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-12-22 21:12:37 +0200

    fixed NaN bug and extracted code to functions for readability

--- src/app/components/WorkOrderComponent/work-order.component.ts
+++ src/app/components/WorkOrderComponent/work-order.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { dateDiffInDays } from 'src/app/helpers/utils';
 import { UserService } from 'src/app/services/UserService/user.service';
 import { WorkOrdersService } from 'src/app/services/WorkOrdersService/work-orders.service';
-import { UserType, WorkOrder } from 'src/app/types/WorkOrder';
 
 @Component({
   selector: 'app-work-order',
@@ -13,24 +13,51 @@ export class WorkOrderComponent implements OnInit {
   userType!: UserType;
   average!: number;
   @Input() workOrder?: WorkOrder;
-  estimatedTime!: number;
+  daysLeftEstimation!: number;
+  tasksStatus = { toDo: 0, inProgress: 0, finished: 0 };
+
   constructor(
     private userService: UserService,
     public workOrdersService: WorkOrdersService,
     private router: Router
   ) {}
 
+  populateVariables() {
+    this.userType = this.userService.user!;
+    this.average = this.workOrdersService.getTotalProgress(
+      this.workOrder?.workItems!
+    );
+    this.daysLeftEstimation = dateDiffInDays(
+      new Date(this.workOrder!.header.startDate),
+      new Date(this.workOrder!.header.endDate)
+    );
+  }
+  populateTaskStatuses = () => {
+    this.workOrder!.workItems.forEach((workItem) => {
+      if (workItem.progress === 0) {
+        this.tasksStatus.toDo += 1;
+      } else if (workItem.progress > 0 && workItem.progress < 100) {
+        this.tasksStatus.inProgress += 1;
+      } else if (workItem.progress === 100) {
+        this.tasksStatus.finished += 1;
+      } else {
+        console.log('This work Item has an invalid progress value');
+      }
+    });
+  };
+
   ngOnInit(): void {
-    if (this.userService.user) {
-      this.userType = this.userService.user;
-      this.average = this.workOrdersService.getTotalProgress(
-        this.workOrder?.workItems!
-      );
-      this.estimatedTime =
-        Number(this.workOrder?.header.endDate.split('-')[2]) -
-        Number(this.workOrder?.header.startDate.split('-')[2]);
-    } else {
-      this.router.navigateByUrl('login');
+    if (!this.userService.user) {
+      this.router.navigateByUrl('login', { replaceUrl: true });
+      return;
     }
+
+    if (!this.workOrder) {
+      this.router.navigateByUrl('404/errorHasOccurred');
+      return;
+    }
+
+    this.populateVariables();
+    this.populateTaskStatuses();
   }
 }
