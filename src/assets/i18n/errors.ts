1caf6d2324d0ff6fa84cad0bf0321aa733393cdb
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-12-22 21:13:17 +0200

    added default error message

--- src/assets/i18n/errors.ts
+++ src/assets/i18n/errors.ts
@@ -2,4 +2,5 @@ export const ERRORS: Record<string, string> = {
   orderNotFound: "This task doesn't exist yet",
   unauthorized: "You don't have access to this resource",
   default: "The page you're looking for doesn't exist.",
+  errorHasOccurred: 'An Error Has Occurred',
 };
